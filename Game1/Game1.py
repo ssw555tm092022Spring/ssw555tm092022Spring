6f873ebde146193d7fe9288e1b25535015b975be
Siddhant Kumar <63947661+siddhantkumar052@users.noreply.github.com>
2022-03-21 22:11:33 -0400

    Update Game1.py
    feat: buttons

--- Game1/Game1.py
+++ Game1/Game1.py
@@ -54,3 +54,39 @@ def button_click(b, number):
             for key in answer_dict:
                 key["text"] = ""
             answer_dict = {}
+
+
+# Define buttons
+b0 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b0, 0))
+b1 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b1, 1))
+b2 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b2, 2))
+b3 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b3, 3))
+b4 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b4, 4))
+b5 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b5, 5))
+b6 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b6, 6))
+b7 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b7, 7))
+b8 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b8, 8))
+b9 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b9, 9))
+b10 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b10, 10))
+b11 = Button(my_frame, text='' , font=("Helvetica",20), height=3, width=6 ,command=lambda: button_click(b11, 11))
+
+#Grid our Buttons
+
+b0.grid(row=0, column=0)
+b1.grid(row=0, column=1)
+b2.grid(row=0, column=2)
+b3.grid(row=0, column=3)
+
+b4.grid(row=1, column=0)
+b5.grid(row=1, column=1)
+b6.grid(row=1, column=2)
+b7.grid(row=1, column=3)
+
+b8.grid(row=2, column=0)
+b9.grid(row=2, column=1)
+b10.grid(row=2, column=2)
+b11.grid(row=2, column=3)
+my_label = Label(root, text = "")
+my_label.pack(pady=20)
+
+root.mainloop()
